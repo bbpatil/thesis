1ac6f867914411fae77e8de408dc8da500a925e1
Franz Profelt <franz.profelt@gmail.com>
2016-04-15 14:08:17 +0200

    setup:
    - added analyze bin folder to PATH during init script

--- InitWork.sh
+++ InitWork.sh
@@ -1,21 +1,26 @@
  #! /bin/bash
  
- echo "load omnet++ environment variables"
+echo "load omnet++ environment variables"
 pushd $OMNETPP_ROOT
- source setenv
- popd
- 
- echo "start omnet++ ide"
- omnetpp &
- 
- echo "open omnet++ documentation"
- google-chrome https://omnetpp.org/documentation &
- 
- echo "open masterthesis"
- texstudio doc/thesis/masterthesis.tex --master &
- 
- echo "open doc folder"
- dolphin doc &
- 
- echo "switch to src folder"
- cd src
+source setenv
+popd
+
+ANALYZE_BIN=$(realpath src/analyze/bin/)
+
+echo "add analyze bin folder to PATH"
+export PATH=$PATH:$ANALYZE_BIN
+
+echo "start omnet++ ide"
+omnetpp &
+
+echo "open omnet++ documentation"
+google-chrome https://omnetpp.org/documentation &
+
+echo "open masterthesis"
+texstudio doc/thesis/masterthesis.tex --master &
+
+echo "open doc folder"
+dolphin doc &
+
+echo "switch to src folder"
+cd src
